8d645984ad569f54b45d00342dbed3ef48e06ef0
LokiFK <60155802+LokiFK@users.noreply.github.com>
2020-10-10 16:31:12 +0200

    Import optimization

--- Backend/src/Datapreprocessing/DataPreprocessing.java
+++ Backend/src/Datapreprocessing/DataPreprocessing.java
@@ -7,7 +7,6 @@ import org.json.simple.parser.ParseException;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
